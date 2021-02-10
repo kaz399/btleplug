a1c7cf1afe7f3889358bf11ea9d72b4af503b471
Andrew Walbran <qwandor@google.com>
2021-02-10 22:33:04 +0000

    Make examples async.

--- examples/event_driven_discovery.rs
+++ examples/event_driven_discovery.rs
@@ -1,28 +1,29 @@
-extern crate btleplug;
-extern crate rand;
-
-use btleplug::api::{bleuuid::BleUuid, Central, CentralEvent};
+use btleplug::api::async_api::Central;
+use btleplug::api::{bleuuid::BleUuid, CentralEvent};
 #[cfg(target_os = "linux")]
-use btleplug::bluez::{adapter::Adapter, manager::Manager};
+use btleplug::bluez_async::{adapter::Adapter, manager::Manager};
 #[cfg(target_os = "macos")]
 use btleplug::corebluetooth::{adapter::Adapter, manager::Manager};
 #[cfg(target_os = "windows")]
 use btleplug::winrtble::{adapter::Adapter, manager::Manager};
+use futures::stream::StreamExt;
+use std::error::Error;
 
 // adapter retrieval works differently depending on your platform right now.
 // API needs to be aligned.
 
-fn get_central(manager: &Manager) -> Adapter {
-    let adapters = manager.adapters().unwrap();
+async fn get_central(manager: &Manager) -> Adapter {
+    let adapters = manager.adapters().await.unwrap();
     adapters.into_iter().nth(0).unwrap()
 }
 
-pub fn main() {
-    let manager = Manager::new().unwrap();
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let manager = Manager::new().await?;
 
     // get the first bluetooth adapter
     // connect to the adapter
-    let central = get_central(&manager);
+    let central = get_central(&manager).await;
 
     // Each adapter can only have one event receiver. We fetch it via
     // event_receiver(), which will return an option. The first time the getter
@@ -33,15 +34,15 @@ pub fn main() {
     // retrieval system in btleplug v0.x while still allowing us to use event
     // streams/channels instead of callbacks. In btleplug v1.x, we'll retrieve
     // channels as part of adapter construction.
-    let event_receiver = central.event_receiver().unwrap();
+    let mut events = central.events().await?;
 
     // start scanning for devices
-    central.start_scan().unwrap();
+    central.start_scan().await?;
 
     // Print based on whatever the event receiver outputs. Note that the event
     // receiver blocks, so in a real program, this should be run in its own
     // thread (not task, as this library does not yet use async channels).
-    while let Ok(event) = event_receiver.recv() {
+    while let Some(event) = events.next().await {
         match event {
             CentralEvent::DeviceDiscovered(bd_addr) => {
                 println!("DeviceDiscovered: {:?}", bd_addr);
@@ -82,4 +83,5 @@ pub fn main() {
             _ => {}
         }
     }
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -20,16 +20,19 @@
 //! An example of how to use the library to control some BLE smart lights:
 //!
 //! ```rust,no_run
+//! use rand::{Rng, thread_rng};
+//! use std::error::Error;
 //! use std::thread;
 //! use std::time::Duration;
-//! use rand::{Rng, thread_rng};
+//! use tokio::time;
 //! #[cfg(target_os = "linux")]
-//! use btleplug::bluez::{adapter::Adapter, manager::Manager};
+//! use btleplug::bluez_async::manager::Manager;
 //! #[cfg(target_os = "windows")]
-//! use btleplug::winrtble::{adapter::Adapter, manager::Manager};
+//! use btleplug::winrtble::manager::Manager;
 //! #[cfg(target_os = "macos")]
-//! use btleplug::corebluetooth::{adapter::Adapter, manager::Manager};
-//! use btleplug::api::{bleuuid::uuid_from_u16, Central, Peripheral, WriteType};
+//! use btleplug::corebluetooth::manager::Manager;
+//! use btleplug::api::async_api::{Central, Peripheral};
+//! use btleplug::api::{bleuuid::uuid_from_u16, WriteType};
 //! use uuid::Uuid;
 //!
 //! const LIGHT_CHARACTERISTIC_UUID: Uuid = uuid_from_u16(0xFFE9);
@@ -37,29 +40,28 @@
 //! // adapter retreival works differently depending on your platform right now.
 //! // API needs to be aligned.
 //!
-//! pub fn main() {
-//!     let manager = Manager::new().unwrap();
+//! #[tokio::main]
+//! async fn main() -> Result<(), Box<dyn Error>> {
+//!     let manager = Manager::new().await.unwrap();
 //!
 //!     // get the first bluetooth adapter
-//!     let adapters = manager.adapters().unwrap();
+//!     let adapters = manager.adapters().await?;
 //!     let central = adapters.into_iter().nth(0).unwrap();
 //!
 //!     // start scanning for devices
-//!     central.start_scan().unwrap();
+//!     central.start_scan().await?;
 //!     // instead of waiting, you can use central.event_receiver() to fetch a channel and
 //!     // be notified of new devices
-//!     thread::sleep(Duration::from_secs(2));
+//!     time::sleep(Duration::from_secs(2)).await;
 //!
 //!     // find the device we're interested in
-//!     let light = central.peripherals().into_iter()
-//!         .find(|p| p.properties().local_name.iter()
-//!             .any(|name| name.contains("LEDBlue"))).unwrap();
+//!     let light = find_light(&central).await.unwrap();
 //!
 //!     // connect to the device
-//!     light.connect().unwrap();
+//!     light.connect().await?;
 //!
 //!     // discover characteristics
-//!     light.discover_characteristics().unwrap();
+//!     light.discover_characteristics().await?;
 //!
 //!     // find the characteristic we want
 //!     let chars = light.characteristics();
@@ -69,9 +71,25 @@
 //!     let mut rng = thread_rng();
 //!     for _ in 0..20 {
 //!         let color_cmd = vec![0x56, rng.gen(), rng.gen(), rng.gen(), 0x00, 0xF0, 0xAA];
-//!         light.write(&cmd_char, &color_cmd, WriteType::WithoutResponse).unwrap();
-//!         thread::sleep(Duration::from_millis(200));
+//!         light.write(&cmd_char, &color_cmd, WriteType::WithoutResponse).await?;
+//!         time::sleep(Duration::from_millis(200)).await;
 //!     }
+//!     Ok(())
+//! }
+//!
+//! async fn find_light<P: Peripheral>(central: &impl Central<P>) -> Option<P> {
+//!     for p in central.peripherals().await.unwrap() {
+//!         if p.properties()
+//!             .await
+//!             .unwrap()
+//!             .local_name
+//!             .iter()
+//!             .any(|name| name.contains("LEDBlue"))
+//!         {
+//!             return Some(p);
+//!         }
+//!     }
+//!     None
 //! }
 //! ```
 
